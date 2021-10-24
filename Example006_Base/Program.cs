6d59f2366249c2acb49e2b83ca378e61ecd60381
Каснер Герман <Axelchannel933@gmail.com>
2021-10-24 15:29:31 +0300

    Add base

---
+++ Example006_Base/Program.cs
@@ -0,0 +1,13 @@
+﻿int a= 1;
+int b= 2;
+int c= 6;
+int d= 8;
+int f= 4;
+int max=a;
+if ( a>max ) max=a;
+if ( b>max ) max=b;
+if ( c>max ) max=c;
+if ( d>max ) max=d;
+if ( f>max ) max=f;
+Console.Write("Максимальное число: ");
+Console.WriteLine(max);
